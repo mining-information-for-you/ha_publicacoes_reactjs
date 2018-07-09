e87c7f51a3628f1fb0348a39802e6915cd1abacb
Bruno Martins <brunomartins@MacBook-Pro-de-Bruno.local>
2018-07-09 11:08:36 -0300

    menu citacoes

--- src/components/LeftContent/LeftContent.js
+++ src/components/LeftContent/LeftContent.js
@@ -10,20 +10,27 @@ class LeftContent extends Component {
         return(
           <div className="left-content">
           <div className="global-links">
-              <a href="javascript:void(0)">
+              <a href="" >
                 <img src="https://avatars1.githubusercontent.com/u/108081?s=400&v=4" /> Rodrigo Faccioli
                 <span className="counter">1</span>
               </a>
 
+             
               <div className="group-content">
                 <h4>Grupos</h4>
-                <a href="">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown" >
+                     <ul class="dropdown-menu">
+                        <li><a href="#">Citacao 01 <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Citacao 02 <span class="glyphicon glyphicon-stats pull-right"></span></a></li>
+                        <li class="divider"></li>
+                     </ul>
                   <img src="http://theieltscoach.com/wp-content/uploads/2015/03/IELTS-Speaking-Tips-Icon-2.png" /> HA Barretos
-                  <span className="counter">1</span>
+                  
                 </a>
                 <a href="">
                   <img src="http://theieltscoach.com/wp-content/uploads/2015/03/IELTS-Speaking-Tips-Icon-2.png" /> Pubmed
-                  <span className="counter">1</span>
+                  
                 </a>
 
               </div>
